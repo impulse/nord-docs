2bb5362256776d86f06e04530d4931b60aada9b5
impulse <ecklf@icloud.com>
2019-07-31 12:38:31 +0200

    Fix typo in vim plugin support

--- src/components/organisms/page/ports/vim/SectionPluginSupport/SectionPluginSupport.jsx
+++ src/components/organisms/page/ports/vim/SectionPluginSupport/SectionPluginSupport.jsx
@@ -106,7 +106,7 @@ const SectionPluginSupport = ({ assets }) => (
         </Visualization>
         <Text>
           <Subline>
-            Beautiful syntax highlighting with support for gr
+            Beautiful syntax highlighting with support for{" "}
             <Link href="https://github.com/fatih/vim-go">vim-go</Link> and{" "}
             <Link href="https://github.com/vim-airline/vim-airline">vim-airline</Link>.
           </Subline>
